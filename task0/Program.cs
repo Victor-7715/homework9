c19c24301eaa7505ccfdb5661dc45537f39057d0
Kozhevnikov Viktor <victor_kozhev@mail.ru>
2022-10-12 02:51:37 +0300

    add task3

--- task0/Program.cs
+++ task0/Program.cs
@@ -5,12 +5,13 @@
 // a = 9, b = -3 -> да
 // a = -3, b = 9 -> нет
 
+// Math.Pow(number,2) - степень
 Console.WriteLine("Введите первое число");
 int numberOne = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numberTwo = int.Parse(Console.ReadLine());
 
-if (numberOne == numberTwo*numberTwo)
+if (numberOne == Math.Pow(numberTwo,2))
 {
     Console.WriteLine("Да");
 }
