f83ad6a495982991f5a5d74a8588c0a08e05ed7b
Kozhevnikov Viktor <victor_kozhev@mail.ru>
2022-10-12 04:34:39 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+
+    Console.WriteLine("Введите первое число ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int num2 = int.Parse(Console.ReadLine());
+
+    if(num1 > num2)
+    {
+        Console.WriteLine("Первое число больше второго ");
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого ");    
+    }
+    
+  
