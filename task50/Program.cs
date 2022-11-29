5be1eadc30d2f2555ef4cd6a0509203dff437826
Kozhevnikov Viktor <victor_kozhev@mail.ru>
2022-11-29 16:31:56 +0300

    add task50-54

---
+++ task50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+Console.WriteLine("Введите количество строк ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numb = new int[lines, columns];
+FillArray(numb);
+PrintArray(numb);
+
+for (int i = 0; i < numb.GetLength(0); i++)
+{
+    for (int j = 0; j < numb.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numb.GetLength(1) - 1; z++)
+        {
+            if (numb[i, z] <  numb[i, z + 1])
+            {
+                int temp = 0;
+                temp = numb[i, z];
+                numb[i, z] = numb[i, z + 1];
+                numb[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями: ");
+PrintArray(numb);
+
+void FillArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
--- task6/Program.cs
+++ task6/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Задача 4: Напишите программу, которая принимает на
 // вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите число ");
+Console.WriteLine("Введите число a ");
 int a1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
 int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c ");
 int c1 = int.Parse(Console.ReadLine());
 
 int max = a1;
@@ -16,4 +18,4 @@ if (c1 > max)
     max = c1;
 } 
 
-Console.WriteLine(max);
+Console.WriteLine("Максимальное число " + max);
